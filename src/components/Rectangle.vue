4606dff5da956dbe84f0d9d420332b8d1f437676
Firstsnow Lucky <firstsnow1119@163.com>
2023-08-20 21:43:32 +0800

    firstsnow

---
+++ src/components/Rectangle.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main">
+        <div class="main-top">
+            <p>``</p>
+            <p>2</p>
+            <p>cnv</p>
+            <p>vd</p>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.main {
+    .main-top {
+        p {
+        }
+    }
+}
+</style>
